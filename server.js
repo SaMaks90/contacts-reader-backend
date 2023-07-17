a80492480e1e0e7a2fc8345ffd8b5cc392742359
SaMaks90 <samchenkoms@gmail.com>
2023-07-17 09:42:56 +0300

    Use secret data in env

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
-// require('dotenv').config();
+require('dotenv').config();
 
 const app = require('./app');
 
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// const DB_HOST = process.env.DB_HOST;
+const DB_HOST = process.env.DB_HOST;
 
 mongoose.set('strictQuery', true);
 
-mongoose.connect('mongodb+srv://user1:gYn1KKahxlWKtegE@mongosb.ae98jge.mongodb.net/contacts_reader?retryWrites=true&w=majority')
-    .then(() => app.listen(3000))
+mongoose.connect(DB_HOST)
+    .then(() => app.listen(PORT))
     .catch(e => {
         console.log(e.message);
         process.exit(1);
